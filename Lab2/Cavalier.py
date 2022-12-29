12c13f4994f3af683012ee305070838da32ccc27
GusevaAnastasia <aguseva.03@mail.ru>
2022-12-29 22:41:56 +0500

    changing cavalier class

--- Lab2/Cavalier.py
+++ Lab2/Cavalier.py
@@ -1,9 +1,10 @@
 from Dog import Dog
 class Cavalier(Dog):
-    def __init__(self, group, country, color, price):
+    def __init__(self, group, country, color, price, health_condition):
         super().__init__(group, country, color)
         self.name = "Cavalier"
         self.price = price
+        self.health_condition = health_condition
     def __repr__(self):
         return f'Dog(name = {self.name}, group = {self.group}, country = {self.country},'\
-               f'color = {self.color}, price = {self.price})'
+               f'color = {self.color}, price = {self.price}, health_condition = {self.health_condition})'
