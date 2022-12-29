9ff283d58695fcb62b45d67bfa52af29499d75cd
GusevaAnastasia <aguseva.03@mail.ru>
2022-12-30 01:35:12 +0500

    main fail creation

---
+++ Lab4/main.py
@@ -0,0 +1,15 @@
+import csv
+from functools import reduce
+
+with open("iris.data",'r') as file:
+        reader = csv.reader(file,delimiter=",")
+        filtered_iterable = filter(lambda row: len(row) != 0 and row[4] == 'Iris-setosa', reader)
+        mapped_iterable = map(lambda row : float(row[2]),filtered_iterable)
+        enumerated_iterable = enumerate(mapped_iterable,1)
+        result = reduce(lambda accumulator, element: (
+        element[0], accumulator[1]+element[1]), enumerated_iterable, (0, 0))
+        average = result[1]/result[0]
+with open("result.txt",'w',encoding="UTF-8") as result_file:
+    result_file.write(str(average))
+
+
