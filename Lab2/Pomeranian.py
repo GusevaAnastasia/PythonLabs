68f3c1b5ca0ffc91b8458ba53a8917b5a2b5cb35
GusevaAnastasia <aguseva.03@mail.ru>
2022-12-29 23:00:03 +0500

    adding max_weight in pomeranian class

--- Lab2/Pomeranian.py
+++ Lab2/Pomeranian.py
@@ -1,10 +1,12 @@
 from Dog import Dog
 class Pomeranian(Dog):
-    def __init__(self, group, color, age):
+    def __init__(self, group, color, age, max_weight):
         super().__init__(group, country, color)
         self.name = "Pomeranian"
         self.age = age
+        self.max_weight = max_weight
     def __repr__(self):
         return f'Dog (name = {self.name}, group = {self.group},'\
-f' country = {self.country} \ color = {self.color}, age = {self.age})'
+f' country = {self.country} \ color = {self.color}, age = {self.age},' \
+f' max_weight = {self.max_weight})'
         
